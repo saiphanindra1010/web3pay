2e47fb1d68e6fd030fd64b83a89964463f014425
saiphanindra1010ddsd <saiphanindra1010@gmail.com>
2024-10-26 19:47:26 +0530

    fix nav

--- web3payfrontend/src/components/Navbar.jsx
+++ web3payfrontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import {Link} from 'react-router-dom'
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
@@ -7,9 +7,9 @@ const Navbar = () => {
     <header className="w-5/6 mx-auto mt-4 p-4 border bg-green-950 bg-opacity-50 backdrop-filter backdrop-blur-lg border-green-700 shadow-lg rounded-lg flex justify-center">
       <nav className="container flex flex-wrap items-center justify-between">
         {/* Logo */}
-        <a href="#" className="text-2xl font-bold text-green-900 dark:text-white">
+        <Link to="/" className="text-2xl font-bold text-green-900 dark:text-white">
           Web3 Payments
-        </a>
+        </Link>
 
         {/* Mobile Menu Button */}
         <button
@@ -32,19 +32,24 @@ const Navbar = () => {
         </button>
 
         {/* Menu Links */}
-        <div className={`w-full sm:flex sm:items-center sm:w-auto ${isNavOpen ? 'block' : 'hidden'}`}>
-          <ul className="flex flex-col sm:flex-row sm:space-x-6 mt-4 sm:mt-0">
-            <li>
-              <a href="#" className="text-green-800 dark:text-white hover:underline transition">Dashboard</a>
-            </li>
-            <li>
-              <a href="#" className="text-green-800 dark:text-white hover:underline transition">API</a>
-            </li>
-            <li>
-              <button type="button" className='bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded'>Sign out</button>
-            </li>
-          </ul>
-        </div>
+        <div className={`w-full sm:flex sm:items-center sm:justify-center sm:w-auto ${isNavOpen ? 'block' : 'hidden'}`}>
+  <ul className="flex flex-col sm:flex-row sm:items-center sm:space-x-6 mt-4 sm:mt-0">
+    <li>
+      <Link to="/" className="text-green-800 dark:text-white hover:underline transition">Dashboard</Link>
+    </li>
+    <li>
+      <Link to="/apicreate" className="text-green-800 dark:text-white hover:underline transition">API Console</Link>
+    </li>
+    <li>
+      <button
+        type="button"
+        className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Sign out
+      </button>
+    </li>
+  </ul>
+</div>
       </nav>
     </header>
   );
